3c0416f203b19a4977f42c0b3b1157226815e454
Tom <johnsonadika@gmail.com>
2022-09-08 10:00:51 -0700

    low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+e file 13 lines (11 sloc) 237 Bytes
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
